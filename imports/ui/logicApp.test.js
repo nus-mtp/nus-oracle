2c427a286dc9e34599ee3fbfbc2359e9f7a8d680
bsweeyee <sweeyee3@gmail.com>
2017-02-05 15:36:06 +0800

    REMOVE test cases for logicApp

--- imports/ui/logicApp.test.js
+++
@@ -1,8 +0,0 @@
-import { assert } from 'meteor/practicalmeteor:chai';
-import { Atom } from './logicApp.js';
-
-describe('logicApp', function () {
-  it('my test', function () {
-    assert.equal(Atom.value, 1);
-  });
-});
