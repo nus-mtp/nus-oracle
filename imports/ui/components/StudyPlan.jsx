d95b1f12c3215afcd4908871f8b303870b3cb443
NatashaKSS <xnagol@gmail.com>
2017-02-06 17:59:59 +0800

    ADDED layout of dashboard

---
+++ imports/ui/components/StudyPlan.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function StudyPlan() {
+  return (
+    <div className="page-content">
+      <div className="container-fluid">
+        <div className="col-xxl-3 col-md-6">
+          StudyPlan
+        </div>
+      </div>
+    </div>
+  );
+}
