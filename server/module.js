c12258df707bf2d3d9eb61c828ab2780ff31ad25
Satrio Triatmoko <tio.triatmoko@outlook.com>
2017-02-09 14:32:59 +0800

    Done a simple module schema and a sample test data

--- server/module.js
+++ server/module.js
@@ -1,22 +1,97 @@
 import { Meteor } from 'meteor/meteor';
+// temporary schema for storing module and term whereby the module is offered
+// TO-DO: check with the front end regarding student + study plan database.
 
-const moduleCollection = new Meteor.Collection('module');
-moduleCollection.schema = new SimpleSchema(
-  {
-    moduleName: {
-      type: String,
+// this function should only be called if there is no module collection yet to
+// be established.
+let termCollection;
+let moduleCollection;
+
+const createModCollection = function createModuleCollection() {
+  termCollection = new Meteor.Collection('termOffered');
+  termCollection.Schema = new SimpleSchema(
+    {
+      termYear: {
+        type: String,
+      },
+      semester: {
+        type: String,
+      },
     },
-    moduleDescription: {
-      type: String,
+  );
+
+  moduleCollection = new Meteor.Collection('module');
+  moduleCollection.schema = new SimpleSchema(
+    {
+      moduleName: {
+        type: String,
+      },
+      moduleDescription: {
+        type: String,
+      },
+      modulePrerequisite: {
+        type: String,
+      },
+      moduleCorequisite: {
+        type: String,
+      },
+      modulePreclusion: {
+        type: String,
+      },
+      termOffered: {
+        type: [Object],
+      },
     },
-    modulePrerequisite: {
-      type: String,
+  );
+};
+
+const testDataNew = function insertTestData() {
+  // always remove previous content before inserting
+  termCollection.remove({});
+  moduleCollection.remove({});
+  // start inserting values
+  termCollection.insert(
+    {
+      termYear: '2016/7',
+      semester: '1',
     },
-    moduleCorequisite: {
-      type: String,
+  );
+
+  termCollection.insert(
+    {
+      termYear: '2016/7',
+      semester: '2',
     },
-    modulePreclusion: {
-      type: String,
+  );
+
+  termCollection.insert(
+    {
+      termYear: '2017/8',
+      semester: '1',
     },
-  },
-);
+  );
+
+  termCollection.insert(
+    {
+      termYear: '2017/8',
+      semester: '2',
+    },
+  );
+
+  const a = termCollection.find({}).fetch();
+  console.log(a);
+  moduleCollection.insert(
+    {
+      moduleName: 'cs1101s',
+      modulePreclusion: 'cs1010, cs1010e, cs1010s, and its equivalent',
+      moduleCorequisite: 'nonez',
+      moduleDescription: 'too Lazy',
+      modulePrerequisite: 'none',
+      termOffered: a,
+    },
+  );
+  console.log(moduleCollection.findOne({}).termOffered);
+};
+
+createModCollection();
+testDataNew();
