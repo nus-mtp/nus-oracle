b27a0a47f5f01c0ad8e25689538ecdf07cd8972d
Walrus <walrus@hotmail.sg>
2017-02-13 19:18:43 +0800

    FIXED Sidebar menu selection

---
+++ imports/ui/components/sidebar_menu/SideTabbedMenu.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+
+export default class SideTabbedMenu extends React.Component {
+constructor(){
+  super();
+
+  this.state = {
+    tabSelectedIndex: 0
+  }
+}
+
+handleSwitchTab(index) {
+  this.setState({tabSelectedIndex: index});
+}
+
+render(){
+  var tabTitleList = this.props.tabTitleList;
+  var activeMenuPanelIndex = this.state.tabSelectedIndex;
+  var menuPanels = this.props.menuPanelsList;
+  var iconList = this.props.iconList;
+
+  return(
+    <div>
+    <nav className="side-menu side-menu-compact">
+      <ul className="side-menu-list">
+        {tabTitleList.map((buttonTitle, index) => {
+          return (
+              <SidebarMenuButton key={index}
+                   buttonTitle={buttonTitle}
+                   onSwitchTab={this.handleSwitchTab.bind(this, index)}
+                   isActiveTab={(this.state.tabSelectedIndex === index)}
+                   buttonIcon={iconList[index]}/>)
+        })}
+      </ul>
+    </nav>
+    {menuPanels[activeMenuPanelIndex]}
+  </div>
+  );
+}
+}
+
+class SidebarMenuButton extends SideTabbedMenu {
+  render() {
+    var buttonIcon = this.props.buttonIcon;
+    return (
+        <li className={'brown' + (this.props.isActiveTab ? ' opened' : '')}
+            onClick={this.props.onSwitchTab}>
+          <a href="#">
+            <i className={"font-icon font-icon-" + buttonIcon}></i>
+            <span className="lbl">{this.props.buttonTitle}</span>
+          </a>
+        </li>
+    );
+  }
+}
+
+SideTabbedMenu.propTypes = {
+  tabTitleList: React.PropTypes.node,
+  menuPanelsList: React.PropTypes.node,
+  iconList: React.PropTypes.node
+}
+
+SidebarMenuButton.propTypes = {
+  isActiveTab: React.PropTypes.bool,
+  onSwitchTab: React.PropTypes.func,
+  buttonTitle: React.PropTypes.string,
+  buttonIcon: React.PropTypes.node
+}
---
+++ imports/ui/components/sidebar_menu/SideTabbedMenuExtension.jsx
@@ -0,0 +1,143 @@
+import React from 'react';
+
+export default class SideTabbedMenuExtension extends React.Component {
+constructor(){
+  super();
+
+  this.state = {
+    tabSelectedIndex: 0
+  }
+}
+
+handleSwitchTab(index) {
+  this.setState({tabSelectedIndex: index});
+}
+
+render(){
+  return(
+    <nav className="side-menu-addl">
+      <header className="side-menu-addl-title">
+        <div className="caption">Filter by Type</div>
+        <i className="font-icon font-icon-burger"></i>
+      </header>
+      <ul className="side-menu-addl-list">
+        <li className="header">
+          <a href="#">
+            <span className="tbl-row">
+              <span className="tbl-cell tbl-cell-caption">Video</span>
+              <span className="tbl-cell tbl-cell-num">16</span>
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span className="tbl-row">
+              <span className="tbl-cell tbl-cell-caption">Action Required</span>
+              <span className="tbl-cell tbl-cell-num">4</span>
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span className="tbl-row">
+              <span className="tbl-cell tbl-cell-caption">Fund YouTuber</span>
+              <span className="tbl-cell tbl-cell-num">1</span>
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span className="tbl-row">
+              <span className="tbl-cell tbl-cell-caption">Hired & Funded</span>
+              <span className="tbl-cell tbl-cell-num">1</span>
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span className="tbl-row">
+              <span className="tbl-cell tbl-cell-caption">Edit Requested</span>
+              <span className="tbl-cell tbl-cell-num">10</span>
+            </span>
+          </a>
+        </li>
+        <li className="divider"></li>
+        <li className="header">
+          <a href="#">
+            <span className="tbl-row">
+              <span className="tbl-cell tbl-cell-caption">Proposals</span>
+              <span className="tbl-cell tbl-cell-num">15</span>
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span className="tbl-row">
+              <span className="tbl-cell tbl-cell-caption">New</span>
+              <span className="tbl-cell tbl-cell-num">4</span>
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span className="tbl-row">
+              <span className="tbl-cell tbl-cell-caption">Declined</span>
+              <span className="tbl-cell tbl-cell-num">10</span>
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span className="tbl-row">
+              <span className="tbl-cell tbl-cell-caption">Changes Requested</span>
+              <span className="tbl-cell tbl-cell-num">1</span>
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span className="tbl-row">
+              <span className="tbl-cell tbl-cell-caption">Changes Declined</span>
+              <span className="tbl-cell tbl-cell-num">2</span>
+            </span>
+          </a>
+        </li>
+        <li className="divider"></li>
+        <li className="header">
+          <a href="#">
+            <span className="tbl-row">
+              <span className="tbl-cell tbl-cell-caption">Approvals</span>
+              <span className="tbl-cell tbl-cell-num">15</span>
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span className="tbl-row">
+              <span className="tbl-cell tbl-cell-caption">Waiting for Approval Long Example</span>
+              <span className="tbl-cell tbl-cell-num">4</span>
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span className="tbl-row">
+              <span className="tbl-cell tbl-cell-caption">Approved / Completed</span>
+              <span className="tbl-cell tbl-cell-num">10</span>
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span className="tbl-row">
+              <span className="tbl-cell tbl-cell-caption">Video in Dispute</span>
+              <span className="tbl-cell tbl-cell-num">11</span>
+            </span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+}
