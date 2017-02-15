827181bb081884d3c61602ae689277f1db3bfa37
NatashaKSS <xnagol@gmail.com>
2017-02-16 02:00:05 +0800

    ADDED module inputs and onClickOutHandler
    * Implemented UI to execute add module function when user clicks out of
    the input box, much like Microsoft Excel or Google Sheets.

--- imports/ui/components/study_plan/Module.jsx
+++ imports/ui/components/study_plan/Module.jsx
@@ -1,17 +1,53 @@
 import React from 'react';
+import ClickOutHandler from 'react-onclickout'
 
 export default class Module extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      moduleCode: ""
+    }
+  }
+
+  updateModuleInput(event) {
+    this.setState({
+      moduleCode: event.target.value
+    });
+  }
+
+  onClickOut() {
+    console.log("Clicked outside!");
+    // this.props.handleAddModule(this.state.moduleCode);
+  }
+
   render() {
     return (
-      <li className="dd-grey dd-item" data-id="1">
-      	<div className="dd-handle">
-          {this.props.moduleCode}
-        </div>
+      <li className="dd-grey dd-item">
+        <ClickOutHandler onClickOut={this.onClickOut.bind(this)}>
+          <tbody>
+            <tr onChange={this.updateModuleInput.bind(this)}>
+              <td>
+                <form className="input-group" style={{width: '100%'}}>
+                  <input type="text" className="form-control"
+                         value={this.state.moduleCode}/>
+                </form>
+              </td>
+            </tr>
+          </tbody>
+        </ClickOutHandler>
       </li>
     );
   }
 }
 
 Module.propTypes = {
-  moduleCode: React.PropTypes.node
+
 }
+
+/*
+// For locking module in afer typing in the name
+<div className="dd-handle">
+  {this.props.moduleCode}
+</div>
+ */
