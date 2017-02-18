91bb8ef2e455110a19ec1cc4659967e6b7c50849
bsweeyee <sweeyee3@gmail.com>
2017-02-18 18:12:26 +0800

    ADD reactive component for SemModulesCard

---
+++ imports/ui/components/study_plan/SemModulesCardContainer.js
@@ -0,0 +1,16 @@
+import { Meteor } from 'meteor/meteor';
+import { createContainer } from 'meteor/react-meteor-data';
+import { getAllModulesInSemester } from '../../../api/crud-controller/semester/methods';
+import SemModulesCard from './SemModulesCard';
+
+export default SemModulesCardContainer = createContainer((props) => {
+  const sem = props.sem;
+  const semesterIndex = props.semesterIndex;
+  const plannerID = props.plannerID;
+
+  const modules = getAllModulesInSemester(semesterIndex, plannerID);
+  return {
+    sem,
+    modules
+  };
+}, SemModulesCard);
