fe76824fc34336d2b3954e9fb2dfaaa8630c3ab8
NatashaKSS <xnagol@gmail.com>
2017-02-18 20:30:39 +0800

    EDITED Implementation of autosuggest
    * Because the original owner of the autosuggest feature decided not to
    support scroll and keypress enter

--- imports/ui/components/study_plan/ModuleSearchBar.jsx
+++ imports/ui/components/study_plan/ModuleSearchBar.jsx
@@ -1,93 +1,48 @@
-import React from 'react';
-import Autosuggest from 'react-autosuggest';
+import React from 'react'
+import Autocomplete from 'react-async-autocomplete'
 
-export default class Module extends React.Component {
+export default class ModuleSearchBar extends React.Component {
   constructor() {
     super();
-
     this.state = {
-      userInput: "",
-      suggestions: []
+      userInput: undefined,
+      selectedModule: ""
     }
   }
 
-  onSuggestionsFetchRequested(value) {
-    console.log("onSuggestionsFetchRequested: " + value + " value: " + value.value);
-    this.setState({suggestions: getSuggestions(value)});
-  };
+  onChange(userInput) {
+    // Fetch data and setItems
+    console.log("onChange: " + userInput);
+  }
 
-  onSuggestionsClearRequested() {
-    this.setState({suggestions: []});
-  };
-
-  // updateModuleInput(event) {
-  // console.log("updateModuleInput: " + event.target.value);
-  //  this.setState({     userInput: event.target.value   });
-  // }
-
-  updateModuleInput(event, {newValue}) {
-    console.log("updateModuleInput event: " + event + " newValue: " + newValue);
-    this.setState({userInput: newValue});
+  // When the user clicks an option or hits enter
+  onSelect(userInput) {
+    console.log("onSelect: " + userInput);
+    // this.setState({
+    //   selectedModule: userInput
+    // });
   }
 
   render() {
-    const value = this.state.userInput;
-    const suggestions = this.state.suggestions;
-    const inputProps = {
-      placeholder: 'Module code',
-      value,
-      onChange: this.updateModuleInput.bind(this)
-    };
-
-    return (<Autosuggest
-      id={this.props.id}
-      inputProps={inputProps}
-      suggestions={suggestions}
-      onSuggestionsFetchRequested={this.onSuggestionsFetchRequested.bind(this)}
-      onSuggestionsClearRequested={this.onSuggestionsClearRequested.bind(this)}
-      getSuggestionValue={getSuggestionValue}
-      renderSuggestion={renderSuggestion}
-      renderSuggestionsContainer={renderSuggestionsContainer}
-      focusFirstSuggestion={true}
-      focusInputOnSuggestionClick={false}/>);
+    return <Autocomplete ItemView={ModuleEntry}
+                         onChange={this.onChange.bind(this)}
+                         onSelect={this.onSelect.bind(this)} />
   }
 }
 
-Module.propTypes = {
-  // ID that identifies this component uniquely. This id also
-  // uniquely defines the Autosuggest box - necessary if
-  // there are many Autosuggest boxes on the screen.
-  id: React.PropTypes.string
-}
+class ModuleEntry extends React.Component {
+  render() {
+    let displayedModuleDetails = this.props.moduleObj;
 
-// ======================================================
-// Helper functions for configuring the Autosuggest React
-// component
-// ======================================================
+    return
+      <div className="user-data">
+        <div>{displayedModuleDetails.code}</div>
+      </div>;
+  }
+}
 
 const languages = [
   {
-    code: 'adrian',
-    year: 1972
-  }, {
-    code: 'benedict',
-    year: 2012
-  }, {
-    code: 'charles',
-    year: 1972
-  }, {
-    code: 'charlie',
-    year: 2012
-  }, {
-    code: 'elmer',
-    year: 1972
-  }, {
-    code: 'emmanuel',
-    year: 2012
-  }, {
-    code: 'elaine',
-    year: 1972
-  }, {
     code: 'honey',
     year: 2012
   }, {
@@ -110,85 +65,3 @@ const languages = [
     year: 2012
   }
 ]
-
-/**
- * Defines how suggestions are displayed as a list to the user.
- *
- * @param  {[string]} value    Value input by the user in the autocomplete
- *                             input box
- * @return {[array]}    List of suggested modules
- */
-const getSuggestions = (userInput) => {
-  console.log("getSuggestions: " + userInput);
-  let inputValue = userInput.value;
-  inputValue = inputValue.trim().toLowerCase(); // Perform string normalization
-  const inputLength = inputValue.length;
-
-  if (inputLength === 0) {
-    // No suggestions given to user if they don't type in
-    // anything
-    return [];
-  } else {
-    // Return elements in the list with beginning letters that
-    // match inputValue
-    return languages.filter((lang) => {
-      return lang.code.toLowerCase().slice(0, inputLength) === inputValue;
-    });
-  }
-};
-
-/**
- * Populates the autocomplete input box when a suggestion has been selected.
- *
- * @param  {[object]} suggestion    Module object representing a suggestion
- * @return {[string]}    String representation of the input element to be
- *                       populated into the autosuggest input box.
- */
-const getSuggestionValue = (suggestion) => {
-  return suggestion.code;
-}
-
-/**
- * Renders the suggestion onto the autosuggest input box.
- *
- * @param  {[object]} suggestion    Module object representing a suggestion
- * @return {[object]}    Custom HTML element for the design of each suggestion
- */
-const renderSuggestion = (suggestion) => {
-  return(
-    <div className="typeahead-item" style={{width: '10.4em'}}>
-      {suggestion.code}
-    </div>
-  );
-}
-
-/**
- * Renders the suggestions container onto the default autosuggest box's style
- * @param  {[array of objs]} children    List of suggestions Autosuggest will
- *                                       pass in
- * @param  {[object]} props    Style props Autosuggest will pass in
- * @return {[object]}          Custom HTML element for the design of a
- *                             suggestion container
- */
-const renderSuggestionsContainer = ({children, ...props}) => {
-  let style = {
-    position:'absolute',
-    zIndex:'3',
-    overflowY: 'auto',
-    height: '8em'
-  }
-  return (
-    <div {...props}>
-      <ul style={style}>
-        {children}
-      </ul>
-    </div>
-  );
-}
-
-/*
-<form className="input-group" style={{width: '100%'}}>
-  <input type="text" className="form-control" value={this.state.moduleCode}
-         onChange={this.updateModuleInput.bind(this)} />
-</form>
-*/
