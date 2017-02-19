f19d4a91532d33b532f93088df952f4d9a6f2a42
NatashaKSS <xnagol@gmail.com>
2017-02-20 01:54:40 +0800

    ADDED delete function for a Module
    Resolves #94. Contributes to #16
    * Generic button for each Module

--- imports/ui/components/study_plan/Module.jsx
+++ imports/ui/components/study_plan/Module.jsx
@@ -1,14 +1,51 @@
 import React from 'react';
+import ModuleButton from './ModuleButton.jsx'
 
 export default class Module extends React.Component {
   constructor() {
     super();
+    this.state = {
+      onMouseOver: false
+    }
+  }
+
+  handleOnMouseEnter(event) {
+    this.setState({ onMouseOver: true });
+  }
+
+  handleOnMouseLeave(event) {
+    this.setState({ onMouseOver: false });
+  }
+
+  handleButtonClick(buttonPressType) {
+    console.log("Button Press type: " + buttonPressType);
+
+    switch(buttonPressType) {
+      case "DELETE":
+        this.props.handleDeleteModule();
+        break;
+      default:
+        console.log("NO VALID BUTTON PRESS TYPE DETECTED.")
+    }
   }
 
   render() {
     return (
-      <li className="dd-grey dd-item">
+      <li className="dd-grey dd-item"
+          onMouseEnter={this.handleOnMouseEnter.bind(this)}
+          onMouseLeave={this.handleOnMouseLeave.bind(this)}>
         <div className="dd-handle" style={{cursor: 'pointer'}}>
+
+          {/* Only want the delete button rendered on hover */}
+          {this.state.onMouseOver ?
+            <ModuleButton
+              icon="font-icon font-icon-trash"
+              displayColor="#505050"
+              onMouseOverColor="#8b2a2a"
+              buttonType="DELETE"
+              onButtonClick={this.handleButtonClick.bind(this)}/> :
+            null
+          }
           {this.props.moduleCode}
         </div>
       </li>
@@ -17,5 +54,9 @@ export default class Module extends React.Component {
 }
 
 Module.propTypes = {
-  moduleCode: React.PropTypes.string
+  // Module code to be rendered
+  moduleCode: React.PropTypes.string,
+
+  // Handler for deleting modules
+  handleDeleteModule: React.PropTypes.func
 }
