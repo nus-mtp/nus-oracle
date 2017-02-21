6efc9a465189cf301ba673d19d149d62f600739f
NatashaKSS <xnagol@gmail.com>
2017-02-21 17:34:39 +0800

    ADDED Add and Del acad year

--- imports/ui/components/study_plan/AcadYrSection.jsx
+++ imports/ui/components/study_plan/AcadYrSection.jsx
@@ -2,21 +2,26 @@ import React from 'react';
 import AcadYrRow from './AcadYrRow.jsx';
 import Button from '../common/Button.jsx';
 
+import { insertNewAcademicYearInPlanner } from '../../../api/crud-controller/semester/methods.js';
+import { deleteAcademicYearInPlanner } from '../../../api/crud-controller/semester/methods.js';
+
 export default class AcadYrSection extends React.Component {
   handleAddAYButtonClick() {
-    console.log("ADDDED AY!!");
+    insertNewAcademicYearInPlanner(this.props.plannerID);
+  }
+
+  handleDelAYButtonClick() {
+    deleteAcademicYearInPlanner(this.props.plannerID);
   }
 
   render() {
     const plannerID = this.props.plannerID;
     const listOfSemesters = this.props.listOfSemesters;
-    const addSemesterButtonIcon =
-      <i className="glyphicon glyphicon-plus"
-         style={{marginTop: "0.15em", marginRight: "0.5em"}}></i>
 
     return (
       <article className="activity-line-item box-typical">
         <div className="activity-line-action-list">
+
           {listOfSemesters.map((semester, rank) => {
             // renders only an academic year which contains 2 semesters
             if (rank % 2 == 0)  {
@@ -26,11 +31,24 @@ export default class AcadYrSection extends React.Component {
                                 plannerID={plannerID} />;
             }
           })}
-          <Button buttonClass="btn btn-inline btn-warning-outline"
-                  style={{position: "relative", left: "40%"}}
-                  buttonText="New Academic Year"
-                  buttonIcon={addSemesterButtonIcon}
-                  onButtonClick={this.handleAddAYButtonClick.bind(this)} />
+
+          <div style={{position: "relative", left: "30%"}}>
+            <Button buttonClass="btn btn-inline btn-primary"
+                    style={{position: "relative"}}
+                    buttonText="Add new Acad Year"
+                    buttonIcon={
+                      <i className="glyphicon glyphicon-plus"
+                         style={{marginTop: "0.15em", marginRight: "0.5em"}}></i>}
+                    onButtonClick={this.handleAddAYButtonClick.bind(this)} />
+            <Button buttonClass="btn btn-inline btn-danger-outline"
+                    style={{position: "relative"}}
+                    buttonText="Delete latest Acad Year"
+                    buttonIcon={
+                      <i className="font-icon font-icon-trash"
+                         style={{marginTop: "0.15em", marginRight: "0.5em"}}></i>}
+                    onButtonClick={this.handleDelAYButtonClick.bind(this)} />
+          </div>
+
         </div>
       </article>
     );
