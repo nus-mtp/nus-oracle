af3430f18c4d9835302444ed3482622d7b4f0f61
NatashaKSS <xnagol@gmail.com>
2017-02-21 22:02:13 +0800

    ADDED Add Study Plan button
    * To prepare for integration with logic
    * Added docs to this React component due to extensive changes being made

--- imports/ui/components/common/TabbedContainer.jsx
+++ imports/ui/components/common/TabbedContainer.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import AcadYrSectionContainer from '../study_plan/AcadYrSectionContainer.js';
+import Button from './Button.jsx';
+import IconButton from './IconButton.jsx';
 
+/**
+ * React Component for a TabbedContainer that holds the user's study plan.
+ * Every study plan is displayed in a panel within the TabbedContainer.
+ * Different study plans may be viewed by selecting its corresponding Tab.
+ */
 export default class TabbedContainer extends React.Component {
   constructor() {
     super();
@@ -11,6 +18,10 @@ export default class TabbedContainer extends React.Component {
 
   handleSwitchTab(index) {
     this.setState({tabSelectedIndex: index});
+  }
+
+  handleAddStudyPlanClick() {
+    console.log("ADD STUDY PLAN!");
   }
 
   render() {
@@ -32,11 +43,24 @@ export default class TabbedContainer extends React.Component {
               {tabTitleList.map((tabTitle, index) => {
                 return (
                     <Tab key={index}
+                         tabWidth="9em"
+                         navSpanClass="nav-link-in"
                          isFirstTab={(index === 0) ? true : false}
                          tabTitle={tabTitle}
                          onSwitchTab={this.handleSwitchTab.bind(this, index)}
                          isActiveTab={(this.state.tabSelectedIndex === index)} />)
               })}
+
+              {/* "Add" button for a study plan */}
+              <Tab tabWidth="3em"
+                   isFirstTab={false}
+                   tabTitle={
+                     <IconButton
+                       icon="glyphicon glyphicon-plus"
+                       style={{margin: '0.6em 0.25em'}}
+                       displayColor="#505050" onMouseOverColor="#2b9cd5"
+                       onButtonClick={this.handleAddStudyPlanClick.bind(this)} />}
+                   />
             </ul>
           </div>
         </div>
@@ -57,17 +81,19 @@ TabbedContainer.propTypes = {
   plannerIDs: React.PropTypes.array
 }
 
-class Tab extends TabbedContainer {
+/**
+ * React Component for each Tab in the TabbedContainer. Click on this Tab to
+ * selects its corresponding study plan.
+ */
+class Tab extends React.Component {
   render() {
-    var widthEachTab = "200px";
-
     return (
       <li className='nav-item'
-          style={(this.props.isFirstTab ? {width: widthEachTab} : {})}
+          style={{float: "left", width: this.props.tabWidth}}
           onClick={this.props.onSwitchTab}>
         <a className={'nav-link' + (this.props.isActiveTab ? ' active' : '')}
-           role='tab' style={{width: widthEachTab}}>
-          <span className='nav-link-in'
+           role='tab'>
+          <span className={this.props.navSpanClass}
                 style={{paddingTop: '8px', paddingBottom: '8px'}}>
             {this.props.tabTitle}
           </span>
@@ -75,4 +101,22 @@ class Tab extends TabbedContainer {
       </li>
     )
   }
+}
+
+Tab.propTypes = {
+  // Title of the tab (max: 15 chars, or a "..." will be rendered in place)
+  tabTitle: React.PropTypes.node,
+
+  // String representation of the width of each tab. Recommended to be in em
+  // units like this, for example: "9em"
+  tabWidth: React.PropTypes.string,
+
+  // String representation of the class of the span tag within a Tab
+  navSpanClass: React.PropTypes.string,
+
+  // Handler for when a user clicks on this Tab
+  onSwitchTab: React.PropTypes.func,
+
+  // Boolean if true means this Tab is currently selected, false otherwise
+  isActiveTab: React.PropTypes.bool
 }
