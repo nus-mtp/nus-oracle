7c2edba8a11f8384382a58eecd2c8273e0ada604
Satrio Triatmoko <tio.triatmoko@outlook.com>
2017-02-23 18:01:11 +0800

    ADD Focus Area Collections and Schema

---
+++ imports/api/database-controller/focus-area/focusArea.js
@@ -0,0 +1,27 @@
+import { Mongo } from 'meteor/mongo';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
+// This script defines the schema and the collection name
+// for Focus Areas.
+
+// Additional Limitation for the object inside the last three
+// elements are governed in the method.
+class FocusAreaCollection extends Mongo.Collection {}
+
+const focusAreaSchema = {
+  name: {
+    type: String
+  },
+  moduleListPrimary: {
+    type: Object
+  },
+  moduleListFourThousands: {
+    type: Object
+  },
+  moduleListNonPrimary: {
+    type: Object
+  }
+}
+
+const FocusArea = new FocusAreaCollection('focusArea');
+FocusArea.attachSchema(focusAreaSchema);
