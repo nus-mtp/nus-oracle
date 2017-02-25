107a04e4ca8abea090dd6171059d0301d4ebda03
Satrio Triatmoko <tio.triatmoko@outlook.com>
2017-02-25 13:44:14 +0800

    ADD collection description to the graduation Requirement database

--- imports/api/database-controller/graduation-requirement/graduationRequirement.js
+++ imports/api/database-controller/graduation-requirement/graduationRequirement.js
@@ -1,16 +1,22 @@
 import { mongo } from 'meteor/mongo';
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 
+/// This Component handles the initialization of the graduation requirement collection
+/// requirementModule should be in he following format :
+/// module code: boolean true/false
+/// e.g : CS1231: false
+/// the boolean is to indicate in the logic section if the following module requirement has been fulfilled
+
 class GraduationRequirementCollection extends Mongo.Collection {}
 
-const GraduationRequirements = new GraduationRequirementCollection('gradiationRequirement');
+const GraduationRequirements = new GraduationRequirementCollection('graduationRequirement');
 
 const gradRequirementSchema = {
   requirementName: {
     type: String
   },
   requirementModules: {
-    type: [object],
+    type: [Object],
     optional: true
   }
 }
