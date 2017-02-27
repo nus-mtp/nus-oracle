b07d2d7d15d57729d8abbd7295565b0d10b1b401
Walrus <walrus@hotmail.sg>
2017-02-27 14:32:48 +0800

    ADD static profile panel and changed menu icons

--- imports/ui/components/sidebar_menu/Sidebar.jsx
+++ imports/ui/components/sidebar_menu/Sidebar.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import SidebarMenu from './SidebarMenu.jsx';
 import SidebarPanel from './SidebarPanel.jsx';
 import PanelRequirements from './PanelRequirements.jsx'
+import PanelProfile from './PanelProfile.jsx'
 
 export default class Sidebar extends React.Component {
   render() {
-    var menuPanelsList = [<SidebarPanel />, <PanelRequirements />, <SidebarPanel />, <SidebarPanel />, <SidebarPanel />];
+    var menuPanelsList = [<PanelProfile />, <PanelRequirements />, <SidebarPanel />, <SidebarPanel />, <SidebarPanel />];
     var tabTitleList = ["My Profile", "Requirements", "Module Bin", "Exempted", "Recommend Me"];
-    var iconList = ["home", "cart", "speed", "users", "comments"];
+    var iconList = ["user", "learn", "view-cascade", "notebook-bird", "lamp"];
     return (
       <div className="my-side-menu">
         <SidebarMenu tabTitleList={tabTitleList}
