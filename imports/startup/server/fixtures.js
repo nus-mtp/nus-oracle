6ce111f9c6ad3df1addba2efe8bbb05725a48830
Satrio Triatmoko <tio.triatmoko@outlook.com>
2017-03-02 16:46:56 +0800

    EDIT the fixtures to include academic cohort method

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -6,6 +6,7 @@ import { Modules } from '../../api/database-controller/module/module';
 import { Planner } from '../../api/crud-controller/planner/planner';
 import { Students } from '../../api/database-controller/student/student';
 import { AcademicCohort } from '../../api/database-controller/AcademicCohort/acadCohort';
+import { createNewCohort } from '../../api/database-controller/AcademicCohort/methods';
 import { moduleInformationParser,
          moduleListParser } from '../../api/database-conversion/moduleInformationParser';
 
@@ -13,7 +14,13 @@ Meteor.startup(() => {
   if (Modules.find({}).count() === 0) {
     populateModuleFixture();
   }
-
+  if (AcademicCohort.find({}).count() === 0) {
+    const academicYear = ['AY 2013/2014','AY 2014/2015', 'AY 2015/2016','AY 2016/2017'];
+    let i = 0 ;
+    for (i = 0; i < academicYear.length; i++){
+      createNewCohort(academicYear[i]);
+    }
+  }
   if (Planner.find({}).count() === 0) {
     const userIDs = '9f91pejfj912ras';
     const plannerNames = ['testPlanner', 'testPlannerTwo'];
