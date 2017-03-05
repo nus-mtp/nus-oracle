f51f789132095bbae76209cbb06de7ddec463243
NatashaKSS <xnagol@gmail.com>
2017-03-06 01:37:54 +0800

    ADDED Account Setup page

--- imports/ui/components/account/acad-details/SetUpAcadDetail.jsx
+++ imports/ui/components/account/acad-details/SetUpAcadDetail.jsx
@@ -1,36 +1,73 @@
 import React from 'react';
 import Select from 'react-select';
 
-import CourseDropdown from './dropdown/CourseDropdown.jsx';
-import AcadCohortDropdown from './dropdown/AcadCohortDropdown.jsx';
-import PrevEduDropdown from './dropdown/PrevEduDropdown.jsx';
+// Import React Components
+import Button from '../../common/Button.jsx';
+import ProgressSteps from '../../common/ProgressSteps.jsx';
 
+// Student-database methods
 import { createNewStudent } from '../../../../api/database-controller/student/methods';
+
+// Import file paths
+import { pathToLogin } from '../../../../startup/client/Router.jsx';
+import { pathToUserDashboard } from '../../../../startup/client/Router.jsx';
+
+const ACAD_COHORT = [
+  { label: 'AY 2012/2013', value:'AY 2012/2013' },
+  { label: 'AY 2013/2014', value:'AY 2013/2014' },
+  { label: 'AY 2014/2015', value:'AY 2014/2015' },
+  { label: 'AY 2015/2016', value:'AY 2015/2016' },
+  { label: 'AY 2016/2017', value:'AY 2016/2017' }
+]
+
+const COURSE_MAJORS =[
+  { label: 'Business Analytics', value: 'business analytics' },
+  { label: 'Computer Science', value: 'computer science' },
+  { label: 'Information Security', value: 'information security' },
+  { label: 'Information System', value: 'information system' },
+  { label: 'Computer Engineering', value: 'computer engineer' }
+]
+
+const PREV_EDUCATION =[
+  { label: 'Junior College', value:'jc' },
+  { label: 'Polytechnic', value: 'poly' },
+  { label: 'Others', value: 'others'}
+]
 
 export default class SetUpAcadDetail extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {cohort: '', course:'', prevEdu: '', doubleCancel: true};
-    this.handleCourseValueChange = this.handleCourseValueChange.bind(this);
-    this.handleAcadValueChange = this.handleAcadValueChange.bind(this);
-    this.handleEduValueChange = this.handleEduValueChange.bind(this);
+    this.state = {
+      cohort: '',
+      course:'',
+      prevEdu: '',
+      doubleCancel: true
+    };
     this.handleCancel = this.handleCancel.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleAcadValueChange(obj) {
-    this.state.cohort = obj.value;
+    this.setState({ cohort: obj.value });
     this.state.doubleCancel = true;
   }
 
   handleCourseValueChange(obj) {
-    this.state.course = obj.value;
+    this.setState({ course: obj.value });
     this.state.doubleCancel = true;
   }
 
   handleEduValueChange(obj) {
-    this.state.prevEdu = obj.value;
+    this.setState({ prevEdu: obj.value });
     this.state.doubleCancel = true;
+  }
+
+  handleClickNext() {
+    console.log("Clicked Next!");
+  }
+
+  handleClickBack() {
+    console.log("Clicked Back!");
   }
 
   handleCancel(event) {
@@ -40,7 +77,7 @@ export default class SetUpAcadDetail extends React.Component {
     } else {
       Bert.alert( 'Returning to main page' , 'danger');
       Meteor.logout();
-      FlowRouter.go('/');
+      FlowRouter.go(pathToLogin);
     }
   }
 
@@ -56,18 +93,66 @@ export default class SetUpAcadDetail extends React.Component {
         Meteor.users.update(userId, { $set: { 'profile.hasSetup': true} } );
       }
       Bert.alert( 'Setup completed!' , 'success' );
-      FlowRouter.go('/app');
+      FlowRouter.go(pathToUserDashboard);
     }
   }
 
   render() {
     return (
-      <div>
-        <AcadCohortDropdown searchable onValueClick={this.handleAcadValueChange}/>
-        <CourseDropdown searchable onValueClick={this.handleCourseValueChange}/>
-        <PrevEduDropdown searchable onValueClick={this.handleEduValueChange}/>
-        <button onClick={this.handleCancel}> Cancel Setup</button>
-        <button onClick={this.handleSubmit}> Finish Setup</button>
+      <div style={{textAlign: 'center'}}>
+        <h1 className="section-heading" style={{color:'#fd6800', margin: '1.5em'}}>
+          <strong>Account Setup</strong>
+        </h1>
+
+        <div className="col-md-6 blockui-element-container-default"
+             style={{float: 'none', margin: '3.5em auto'}}>
+  				<div className="form-group"
+               style={{color: '#ffffff', marginBottom: '2.5em'}}>
+            <h5 style={{marginBottom: '0.3em'}}>
+              <strong>Academic Cohort</strong>
+            </h5>
+            <Select placeholder="Select"
+                    multi={false} clearable={false} searchable={false}
+                    options={ACAD_COHORT} value={this.state.cohort}
+                    onChange={this.handleAcadValueChange.bind(this)} />
+  				</div>
+
+  				<div className="form-group"
+               style={{color: '#ffffff', marginBottom: '2.5em'}}>
+            <h5 style={{marginBottom: '0.3em'}}>
+              <strong>Course Major</strong>
+            </h5>
+            <Select placeholder="Select"
+                    multi={false} clearable={false} searchable={false}
+                    options={COURSE_MAJORS} value={this.state.course}
+                    onChange={this.handleCourseValueChange.bind(this)} />
+  				</div>
+
+  				<div className="form-group"
+               style={{color: '#ffffff', marginBottom: '2.5em'}}>
+            <h5 style={{marginBottom: '0.3em'}}>
+              <strong>Previous Education</strong>
+            </h5>
+            <Select placeholder="Select"
+                    multi={false} clearable={false} searchable={false}
+                    options={PREV_EDUCATION} value={this.state.prevEdu}
+                    onChange={this.handleEduValueChange.bind(this)} />
+  			  </div>
+
+          <div className="row">
+            <Button buttonClass="btn btn-rounded btn-inline btn-secondary-outline"
+              buttonText="← Back"
+              onButtonClick={this.handleClickBack.bind(this)} />
+
+            <Button buttonClass="btn btn-rounded btn-inline btn-warning-outline"
+              buttonText="Next →"
+              onButtonClick={this.handleClickNext.bind(this)} />
+          </div>
+          <div className="row">
+            <ProgressSteps />
+          </div>
+
+        </div>
       </div>
     );
   }
