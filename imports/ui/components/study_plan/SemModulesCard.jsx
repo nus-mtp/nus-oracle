48a152a9e223f6d7e28da6fa4d9b902a1b236482
NatashaKSS <xnagol@gmail.com>
2017-03-07 00:54:23 +0800

    ADDED Virtualized select component
    * Async loading
    * Can handle heavy data requests

--- imports/ui/components/study_plan/SemModulesCard.jsx
+++ imports/ui/components/study_plan/SemModulesCard.jsx
@@ -1,17 +1,28 @@
 import _ from 'underscore';
 import React from 'react';
+
+// Import React Components
 import Module from './Module.jsx';
 import ModuleSearchBar from './ModuleSearchBar.jsx';
+import VirtualizedSelect from 'react-virtualized-select';
 
 import { insertOneModuleInSemester } from '../../../api/crud-controller/module/methods.js';
 import { deleteOneModuleInSemester } from '../../../api/crud-controller/module/methods.js';
 
+import { CITIES } from './city-examples.js';
 /**
  * React Component that implements the container for a semester's worth of
  * modules - all list in columnal form with rows of Modules. Also has a
  * autosuggest box for searching for modules to be added to the user's study plan.
  */
 export default class SemModulesCard extends React.Component {
+  constructor() {
+      super();
+      this.state = {
+        selectedModule: ''
+      }
+  }
+
   /**
    * Handles the event when a module is added
    **/
@@ -27,9 +38,17 @@ export default class SemModulesCard extends React.Component {
       this.props.semesterIndex, moduleCode, this.props.plannerID);
   }
 
-  render() {
-    const modules = this.props.modules;
+  updateValue(value) {
+    this.setState({
+      selectedModule: value
+    });
+  }
 
+  getModulesList() {
+    return CITIES;
+  }
+
+  render() {
     return (
       <div className="col-md-4">
         <div className="card-grid-col">
@@ -37,13 +56,18 @@ export default class SemModulesCard extends React.Component {
   					<div className="card-typical-section card-typical-content">
 
               {/* Renders all modules from the user's study plan */}
-              {Object.keys(modules).map((moduleCode, index) => {
+              {Object.keys(this.props.modules).map((moduleCode, index) => {
                 return <Module key={index} moduleCode={moduleCode}
                                handleDeleteModule={
-                                 this.handleDeleteModule.bind(this, moduleCode)}/>;
+                                 this.handleDeleteModule.bind(this, moduleCode)} />;
               })}
 
-              <ModuleSearchBar handleAddModule={this.handleAddModule.bind(this)} />
+              {/* <ModuleSearchBar handleAddModule={this.handleAddModule.bind(this)} /> */}
+              <VirtualizedSelect simpleValue searchable
+                name="Module Code" labelKey="name" valueKey="name"
+                value={this.state.selectedModule}
+                onChange={this.updateValue.bind(this)}
+                clearable={false} options={CITIES} />
 
             </div>
   					<div className="card-typical-section">
