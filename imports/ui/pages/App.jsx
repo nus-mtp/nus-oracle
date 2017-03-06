b812b58348d92cbfabebd64279952f8dc42bbdf9
NatashaKSS <xnagol@gmail.com>
2017-03-06 13:47:04 +0800

    FIXED bug where Router could not find App component

--- imports/ui/pages/App.jsx
+++ imports/ui/pages/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import DashboardContainer from '../pages/DashboardContainer';
 
-export class App extends React.Component {
+export default class App extends React.Component {
   render() {
     return (
       <div>
