2eb31c6f210c157b3357c96dbbd14721de0db7b9
NatashaKSS <xnagol@gmail.com>
2017-03-08 00:24:50 +0800

    ADDED new async search bar (yet to try with >7000 entries)

--- imports/ui/components/study_plan/SemModulesCard.jsx
+++ imports/ui/components/study_plan/SemModulesCard.jsx
@@ -1,15 +1,13 @@
-import _ from 'underscore';
 import React from 'react';
 
 // Import React Components
 import Module from './Module.jsx';
-import ModuleSearchBar from './ModuleSearchBar.jsx';
 import VirtualizedSelect from 'react-virtualized-select';
 
 import { insertOneModuleInSemester } from '../../../api/crud-controller/module/methods.js';
 import { deleteOneModuleInSemester } from '../../../api/crud-controller/module/methods.js';
+import { sendQuery } from '../../../api/searcher-controller/controller.js'
 
-import { CITIES } from './city-examples.js';
 /**
  * React Component that implements the container for a semester's worth of
  * modules - all list in columnal form with rows of Modules. Also has a
@@ -18,9 +16,6 @@ import { CITIES } from './city-examples.js';
 export default class SemModulesCard extends React.Component {
   constructor() {
       super();
-      this.state = {
-        selectedModule: ''
-      }
   }
 
   /**
@@ -34,21 +29,56 @@ export default class SemModulesCard extends React.Component {
    * Handles the event when a module is deleted
    */
   handleDeleteModule(moduleCode) {
-    deleteOneModuleInSemester(
-      this.props.semesterIndex, moduleCode, this.props.plannerID);
+    deleteOneModuleInSemester(this.props.semesterIndex, moduleCode, this.props.plannerID);
   }
 
-  updateValue(value) {
-    this.setState({
-      selectedModule: value
+  /**
+   * Handles the event when a user selects a module code from the dropdown.
+   * Even if the user presses Enter, an appropriate module code, the module
+   * code sent by the server itself, will be added to the study plan.
+   *
+   * @param input {[String]}    User's input. Updates as the user types.
+   * @param callback {[func]}    Callback to invoke once async call is done.
+   *
+   * TODO: This might not work for large >7000 modules list
+   */
+  getModulesListFromDB(input, callback) {
+    input = input.trim().toLowerCase();
+
+    let fetchedListOfModulesFromDB = [];
+    if (input.length !== 0) { // Only fetch if user typed in something
+      // Get suggestions from modules DB which best matches input
+      fetchedListOfModulesFromDB = sendQuery(input);
+
+      // Check to see what fetchedListOfModulesFromDB contains
+      // console.log("listOfModulesFromDB: " +
+      //   fetchedListOfModulesFromDB.map((module) => {return JSON.stringify(module);}));
+    }
+
+    // Callback for an async call. This tells us we're done with the call,
+    // and ready to pass VirtualizedSelect the search options to render
+    callback(null, {
+      options: fetchedListOfModulesFromDB,
+      complete: true
     });
   }
 
-  getModulesList() {
-    return CITIES;
+  /**
+   * Handles the event when a user selects a module code from the dropdown.
+   * Even if the user presses Enter, an appropriate module code, the module
+   * code sent by the server itself, will be added to the study plan.
+   *
+   * @param {[Object]}    Module object containing valid fields
+   */
+  handleSelectModuleCode(moduleObj) {
+    if (moduleObj) {
+      this.handleAddModule(moduleObj.moduleCode);
+    }
   }
 
   render() {
+    const modules = this.props.modules;
+
     return (
       <div className="col-md-4">
         <div className="card-grid-col">
@@ -56,19 +86,18 @@ export default class SemModulesCard extends React.Component {
   					<div className="card-typical-section card-typical-content">
 
               {/* Renders all modules from the user's study plan */}
-              {Object.keys(this.props.modules).map((moduleCode, index) => {
+              {Object.keys(modules).map((moduleCode, index) => {
                 return <Module key={index} moduleCode={moduleCode}
                                handleDeleteModule={
                                  this.handleDeleteModule.bind(this, moduleCode)} />;
               })}
 
-              {/* <ModuleSearchBar handleAddModule={this.handleAddModule.bind(this)} /> */}
-              <VirtualizedSelect simpleValue searchable clearable={false} options={CITIES}
-                labelKey="name" placeholder="Add Module..."
-                value={this.state.selectedModule} focusedOption={{ cursor: 'pointer' }}
-                onChange={this.updateValue.bind(this)} />
-
+              <VirtualizedSelect searchable async clearable={false}
+                labelKey="moduleCode" placeholder="Add a module..."
+                loadOptions={this.getModulesListFromDB.bind(this)}
+                onChange={this.handleSelectModuleCode.bind(this)} />
             </div>
+
   					<div className="card-typical-section">
   						<div className="card-typical-linked">
                 {this.props.sem}
