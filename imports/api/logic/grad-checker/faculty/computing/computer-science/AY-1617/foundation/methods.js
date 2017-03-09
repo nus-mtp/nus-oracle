4b254e65011c5756447546490627f75af01f468f
bsweeyee <sweeyee3@gmail.com>
2017-03-09 11:43:08 +0800

    ADD print statement to check for location of error in travis

--- imports/api/logic/grad-checker/faculty/computing/computer-science/AY-1617/foundation/methods.js
+++ imports/api/logic/grad-checker/faculty/computing/computer-science/AY-1617/foundation/methods.js
@@ -19,6 +19,7 @@ export const findFoundationRequirementModules = function findFoundationRequireme
     for (var i=0; i<keyNames.length; i++) {
     // check equivalent module fulfilment if available
     moduleFulfilment = getModuleFulfilment(keyNames[i]);
+    console.log(moduleFulfilment);
 
     moduleFulfilmentMappingEquivalent = moduleFulfilment.moduleMapping[academicCohort].moduleEquivalent;
     markedFoundationModulesAndMCs = markModules(markedFoundationModulesAndMCs, studentSemesters, keyNames[i], keyNames[i]);
