54370bf1f19b616f8158a83ec9113a40b71e6a35
bsweeyee <sweeyee3@gmail.com>
2017-03-09 15:24:32 +0800

    ADD safety checks for student exempted and waived module

--- imports/api/database-controller/student/methods.js
+++ imports/api/database-controller/student/methods.js
@@ -82,7 +82,17 @@ export const updateStudentPreviousEducation = function updateStudentEducation(pr
 
 export const getStudentExemptedModules = function getExemptedModule() {
   const studentID = getStudentID();
-  return getCurrentStudentDocument(studentID).studentExemptedModule;
+  if (!studentID) {
+    return {};
+  }
+  const student = getCurrentStudentDocument(studentID);
+  if (!student)  {
+    return {};
+  }
+  if (!student.studentExemptedModule)  {
+    return {};
+  }
+  return student.studentExemptedModule;
 }
 
 export const addStudentExemptedModule = function addStudentExemptedModule(exemptedModule, callback) {
@@ -124,7 +134,17 @@ export const deleteStudentExemptedModule = function deleteStudentExemptedModule(
 
 export const getStudentWaivedModules = function getStudentAcademicCohort() {
   const studentID = getStudentID();
+  if (!studentID) {
+    return {};
+  }
   const student = getCurrentStudentDocument(studentID);
+  if (!student)  {
+    return {};
+  }
+
+  if (!student.studentWaivedModule)  {
+    return {};
+  }
   return student.studentWaivedModule;
 }
 
