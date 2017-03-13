a4e61c699cd712ec76f4767e700062b7e07eb193
bsweeyee <sweeyee3@gmail.com>
2017-03-13 16:46:42 +0800

    ADD find4KRequirementModules in focusArea

--- imports/api/logic/grad-checker/faculty/computing/computer-science/AY-1617/breadth-depth/focus-area/methods.js
+++ imports/api/logic/grad-checker/faculty/computing/computer-science/AY-1617/breadth-depth/focus-area/methods.js
@@ -1,19 +1,42 @@
 
-export const findFocusAreaPrimaryModules = function findFocusAreaPrimaryModules(academicCohort, studentFocusArea, studentPlanner, exemptedModules, waivedModules)  {
-  // find focus area primary document given academic cohort and student focus area
+export const findFocusAreaPrimaryModules = function findFocusAreaPrimaryModules(academicCohort, studentFocusArea, studentSemesters, exemptedModules, waivedModules, requiredMCs)  {
+  let markedFocusAreaPrimaryModulesAndMCs = {
+    markedFocusAreaPrimaryModules: studentFocusArea,
+    numberOfFocusAreaPrimaryModulesMarkedTrue: 0,
+    totalModuleMCs: 0,
+    moduleChecked: {},
+    requiredMCs: requiredMCs
+  };
 
-  // loop through focus area primary modules
-    // check equivalent module fulfilment available
+  let moduleFulfilment = {};
+  let moduleFulfilmentMappingEquivalent = [];
+  const keyNames = Object.keys(studentFocusArea);
 
-    // check if equivalent module exists in studentPlanner
+  // loop through markedIndustrialExperienceTrainingModules
+    for (var i=0; i < keyNames.length; i++) {
+    // check equivalent module fulfilment if available
+    moduleFulfilment = getModuleFulfilment(keyNames[i]);
 
-    // check if equivalent module exists in exemptedModules
+    moduleFulfilmentMappingEquivalent = moduleFulfilment.moduleMapping[academicCohort].moduleEquivalent;
+    markedFocusAreaPrimaryModulesAndMCs = markExceptions(markedFocusAreaPrimaryModulesAndMCs, studentSemesters, keyNames[i], keyNames[i]);
+    markedFocusAreaPrimaryModulesAndMCs = markExemptedWaivedExceptions(markedFocusAreaPrimaryModulesAndMCs, exemptedModules, waivedModules, keyNames[i], keyNames[i]);
 
-    // check if equivalent module exists in exemptedModules
-
-    // mark focusAreaPrimaryModules as true or false depending on whether modules exists in studentPlanner/exemptedModules/waivedModules
-
+    if (!markedFocusAreaPrimaryModulesAndMCs.markedFocusAreaPrimaryModules[keyNames[i]]
+        && moduleFulfilmentMappingEquivalent.length !== 0) {
+      for (var j = 0; j < moduleFulfilmentMappingEquivalent.length; j++)  {
+        // check if equivalent module exists in studentPlanner, exemptedModules, waivedModules
+        // checks if in exempted or waived modules
+        markedFocusAreaPrimaryModulesAndMCs = markExemptedWaivedExceptions(markedFocusAreaPrimaryModulesAndMCs, exemptedModules, waivedModules, moduleFulfilmentMappingEquivalent[j], keyNames[i]);
+        markedFocusAreaPrimaryModulesAndMCs = markExceptions(markedFocusAreaPrimaryModulesAndMCs, studentSemesters, moduleFulfilmentMappingEquivalent[j], keyNames[i]);
+      }
+    }
+    if (markedFocusAreaPrimaryModulesAndMCs.numberOfFocusAreaPrimaryModulesMarkedTrue === keyNames.length) {
+      markedFocusAreaPrimaryModulesAndMCs.requiredMCs = markedFocusAreaPrimaryModulesAndMCs.totalModuleMCs;
+      break;
+    }
+  }
   // return { moduleCode: boolean } object
+  return markedFocusAreaPrimaryModulesAndMCs;
 }
 
 export const findFocusArea4000Modules = function findFocusArea4000Modules(academicCohort, studentFocusArea, studentPlanner, exemptedModules, waivedModules)  {
@@ -31,4 +54,51 @@ export const findFocusArea4000Modules = function findFocusArea4000Modules(academ
     // mark focusArea4000Modules as true or false depending on whether modules exists in studentPlanner/exemptedModules/waivedModules
 
   // return { moduleCode: boolean } object
+}
+
+const markModules = function markModules(markedFocusAreaModulesAndMCs, studentSemesters, equivalentModule, originalModule) {
+  for (var i = 0; i < studentSemesters.length; i++) {
+    if (studentSemesters[i].moduleHashmap[equivalentModule]) {
+      markedFocusAreaModulesAndMCs.markedFocusAreaPrimaryModules[originalModule] = true;
+      markedFocusAreaModulesAndMCs.numberOfFocusAreaPrimaryModulesMarkedTrue += 1;
+      if (!markedFocusAreaModulesAndMCs.moduleChecked[equivalentModule])  {
+        markedFocusAreaModulesAndMCs.moduleChecked[equivalentModule] = true;
+        markedFocusAreaModulesAndMCs.totalModuleMCs += searchByModuleCode(equivalentModule).moduleMC;
+      }
+      break;
+    }
+  }
+
+  return markedFocusAreaModulesAndMCs;
+}
+
+const markExemptedWaivedModules = function markExemptedWaivedModules(markedFocusAreaModulesAndMCs, exemptedModules, waivedModules, equivalentModule, originalModule) {
+  if (Object.keys(exemptedModules).length !== 0)  {
+    if (exemptedModules[equivalentModule])  {
+      markedFocusAreaModulesAndMCs.markedFocusAreaPrimaryModules[originalModule] = true;
+      markedFocusAreaModulesAndMCs.numberOfFocusAreaPrimaryModulesMarkedTrue += 1;
+      if (!markedFocusAreaModulesAndMCs.moduleChecked[equivalentModule])  {
+        markedFocusAreaModulesAndMCs.moduleChecked[equivalentModule] = true;
+        markedFocusAreaModulesAndMCs.totalModuleMCs += searchByModuleCode(equivalentModule).moduleMC;
+      }
+    }
+  }
+  if (Object.keys(waivedModules).length !== 0)  {
+    if (waivedModules[equivalentModule]) {
+      markedFocusAreaModulesAndMCs.markedFocusAreaPrimaryModules[originalModule] = true;
+      markedFocusAreaModulesAndMCs.numberOfFocusAreaPrimaryModulesMarkedTrue += 1;
+      if (!markedFocusAreaModulesAndMCs.moduleChecked[equivalentModule])  {
+        markedFocusAreaModulesAndMCs.moduleChecked[equivalentModule] = true;
+      }
+    }
+  }
+  return markedFocusAreaModulesAndMCs;
+}
+
+const markExceptions = function markExceptions(markedFocusAreaModulesAndMCs, studentSemesters, equivalentModule, originalModule)  {
+
+}
+
+const markExemptedWaivedExceptions = function markExemptedWaivedExceptions(markedIndustrialExperienceTrainingModulesAndMCs, exemptedModules, waivedModules, equivalentModule, originalModule)  {
+
 }
