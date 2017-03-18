ea08f2bdf1ccd7928ed94c71416ec5fc9666e8d7
NatashaKSS <xnagol@gmail.com>
2017-03-18 13:44:28 +0800

    ADDED High performance module search bar
    * Can search 10,000+ options with <0.1s response time per character user
    typed in
    * RESOLVES #251

--- imports/ui/components/study_plan/SemModulesCard.jsx
+++ imports/ui/components/study_plan/SemModulesCard.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Session } from 'meteor/session';
 
 // Import React Components
 import Module from './Module.jsx';
 import VirtualizedSelect from 'react-virtualized-select';
+import createFilterOptions from 'react-select-fast-filter-options';
 
 // Import necessary constants
 import { SMALL_LINE_CHAR_LIMIT } from '../common/Constants.js';
@@ -11,18 +13,40 @@ import { SMALL_LINE_HEIGHT } from '../common/Constants.js';
 import { MEDIUM_LINE_HEIGHT } from '../common/Constants.js';
 import { LARGE_LINE_HEIGHT } from '../common/Constants.js';
 
+// Import Logic Controller Methods
 import { insertOneModuleInSemester } from '../../../api/crud-controller/module/methods.js';
 import { deleteOneModuleInSemester } from '../../../api/crud-controller/module/methods.js';
 import { sendQuery } from '../../../api/searcher-controller/controller.js'
+import { getAllModules } from '../../../api/searcher-controller/controller.js'
+
+// Import fixtures acting as a local database for all modules
+import { getModuleFixtures } from './fixtures/module_fixtures.js';
+
+
+/**
+ * Populating the module database for this React component on start up.
+ *
+ * If no DB is found, we populate the local DB with module fixtures
+ */
+let allModules = getAllModules(); // If we managed to retrieve all mods from DB
+if (allModules.length === 0) {
+  // If no modules were retrieved from the DB, use fixtures to act as local DB
+  allModules = getModuleFixtures();
+}
+
+// Prepare filter options for fast render and search in search bar
+const filterOptions = createFilterOptions({ options: allModules });
 
 /**
  * React Component that implements the container for a semester's worth of
- * modules - all list in columnal form with rows of Modules. Also has a
- * autosuggest box for searching for modules to be added to the user's study plan.
+ * modules - all lists are in columnal form with rows of Modules.
+ *
+ * Also contains a search bar for fast-searching modules to be added to the
+ * user's study plan.
  */
 export default class SemModulesCard extends React.Component {
   constructor() {
-      super();
+    super();
   }
 
   /**
@@ -53,45 +77,22 @@ export default class SemModulesCard extends React.Component {
   }
 
   /**
-   * Handles the event when a user selects a module code from the dropdown.
-   * Even if the user presses Enter, an appropriate module code, the module
-   * code sent by the server itself, will be added to the study plan.
+   * Calculates the height of each option in the search bar based on the
+   * number of characters in the module code and name.
    *
-   * @param input {[String]}    User's input. Updates as the user types.
-   * @param callback {[func]}    Callback to invoke once async call is done.
+   * This is a workaround for the issue faced in the react-select library
+   * where the height of each dropdown option isn't dynamic and must be set
+   * prior to rendering the search bar.
+   *
+   * Note: The constants used here are very conservative to cater to smaller
+   *       screens.
+   *
+   * @param {[Object]}    Module object containing valid fields
    */
-  getModulesListFromDB(input, callback) {
-    input = input.trim().toLowerCase();
-
-    let fetchedListOfModulesFromDB = [];
-    if (input.length !== 0) { // Only fetch if user typed in something
-      // Get suggestions from modules DB which best matches input
-      /* Returns module object in the following format:
-      {
-        moduleCodeAndName: moduleCode + " " + moduleName,
-        moduleCode: moduleCode,
-        moduleName: moduleName,
-        moduleID: _id,
-      }
-       */
-      fetchedListOfModulesFromDB = sendQuery(input);
-    }
-
-    // Callback for an async call. This tells us we're done with the call,
-    // and ready to pass VirtualizedSelect the search options to render
-    callback(null, {
-      options: fetchedListOfModulesFromDB,
-      complete: true
-    });
-  }
-
   computeLineHeight(option) {
-    const small = 45;
-    const medium = 60;
-    const large = 75;
-
     let module = option.option;
-    let moduleCodeAndNameLen = module.moduleCodeAndName.length;
+    let moduleCodeAndName = module.moduleCode + " " + module.moduleName;
+    let moduleCodeAndNameLen = moduleCodeAndName.length;
 
     if (moduleCodeAndNameLen <= SMALL_LINE_CHAR_LIMIT) {
       return SMALL_LINE_HEIGHT;
@@ -115,21 +116,24 @@ export default class SemModulesCard extends React.Component {
 					<div className="card-typical-section card-typical-content"
                style={{padding: '0.75em'}}>
             <div style={{paddingBottom: '0.75em'}}>
+
               {/* Renders all modules from the user's study plan */}
               {Object.keys(modules).map((moduleCode, index) => {
                 return <Module key={index} moduleCode={moduleCode}
                                handleDeleteModule={
                                  this.handleDeleteModule.bind(this, moduleCode)} />;
               })}
+
             </div>
 
-            {/* Async Search bar to retrieve thousands of records optionHeight: 45px */}
-            <VirtualizedSelect searchable async clearable={false}
-              labelKey="moduleCodeAndName"
-              placeholder="Add a module..."
+            {/* Search bar to retrieve thousands of records */}
+            <VirtualizedSelect
+              placeholder="Add a module..." noResultsText="No modules found"
+              openOnFocus={true} tabSelectsValue={false}
+              options={allModules}
+              filterOptions={filterOptions}
               menuBuffer={50}
               optionHeight={this.computeLineHeight}
-              loadOptions={this.getModulesListFromDB.bind(this)}
               onChange={this.handleSelectModuleCode.bind(this)} />
 
           </div>
