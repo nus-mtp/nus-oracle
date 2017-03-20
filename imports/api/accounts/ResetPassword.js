a20041e21b2b6503557ddf960896fc2c583859c0
Adrian <A0122061@u.nus.edu>
2017-03-20 19:47:37 +0800

    transfer account locking to server side

--- imports/api/accounts/ResetPassword.js
+++ imports/api/accounts/ResetPassword.js
@@ -1,6 +1,13 @@
 // for loginaccount.jsx to reset wrong password
 Meteor.methods({
-  resetpassword(userId) {
-      return Accounts.setPassword(userId, newPassword);
+  resetpassword(email) {
+    let userAccount = Accounts.users.findOne({username: email});
+    const userId = userAccount._id;
+    console.log("asd");
+    Meteor.users.update(userId, {
+      $set: { "profile.accountLock" : true }
+    });
+    return userId;
+      //return Accounts.setPassword(userId, newPassword);
   }
 });
--- imports/startup/server/accounts-setup.js
+++ imports/startup/server/accounts-setup.js
@@ -1,9 +1,13 @@
 import '../../api/accounts/emailverifytemplate';
 import '../../api/accounts/NusEmailVerifier';
-import '../../api/accounts/ResetPassword';
+import '../../api/accounts/lockAccount';
 import '../../api/accounts/send-verification';
 
 Meteor.publish('user-profile', function() {
   //'console.log(Meteor.users.find({_id : this.userId}, {fields : {profile : 1}}).fetch()[0]);
   return Meteor.users.find({_id : this.userId}, {fields : {profile : 1}});
+});
+
+Meteor.publish("allUsers", function () {
+  return Meteor.users.find({});
 });
