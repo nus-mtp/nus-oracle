55dad0f65806a547d3338f6b115359759969ceec
Walrus <walrus@hotmail.sg>
2017-04-06 17:17:23 +0800

    FIX requirements panel not showing loading component

--- imports/ui/components/sidebar_menu/PanelRequirements.jsx
+++ imports/ui/components/sidebar_menu/PanelRequirements.jsx
@@ -2,18 +2,29 @@ import React from 'react';
 import PanelHeader from '../common/PanelHeader.jsx'
 import Nestable from '../common/Nestable.jsx'
 import GradCheckerContainer from './GradCheckerContainer.js'
+import GridLoader from '../common/halogen/GridLoader.js';
 import * as constants from '../common/Constants.js';
 
 export default class PanelRequirements extends React.Component {
   constructor(){
     super();
+    this.state = {isGradCheckerLoaded: false};
+  }
+
+  componentDidMount(){
+    this.setState({isGradCheckerLoaded: true});
+  }
+
+  renderGradChecker(){
+    return(
+    <GradCheckerContainer activePlannerId={this.props.activePlannerId}/>);
   }
 
   render() {
     return (
       <nav className="side-menu-addl">
         <PanelHeader title="CS Degree Requirements" icon="font-icon font-icon-page" />
-        <GradCheckerContainer activePlannerId={this.props.activePlannerId}/>
+        {this.state.isGradCheckerLoaded ? this.renderGradChecker(): <GridLoader color="#ff6600" />}
       </nav>
     );
   }
