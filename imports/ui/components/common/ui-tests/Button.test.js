5e7c0ce8034fe6a94d643675276b1dcadfd2972d
NatashaKSS <xnagol@gmail.com>
2017-04-08 03:17:57 +0800

    ADDED test case for <Button />

--- imports/ui/components/common/ui-tests/Button.test.js
+++ imports/ui/components/common/ui-tests/Button.test.js
@@ -1,25 +1,47 @@
 import React from 'react';
 import { assert, expect } from 'meteor/practicalmeteor:chai';
 import { mount, shallow } from 'enzyme';
+import 'jsdom-global/register'; // Mock load a global HTML doc
 
 import Button from '../Button.jsx';
 
 describe('<Button />', function () {
-  it ('should have props for buttonClass', function() {
+  it('renders without any error', () => {
+    expect(shallow(<Button />));
+  })
+
+  it ('should have props defined', function() {
     const wrapper = shallow(<Button />);
     expect(wrapper.props().buttonClass).to.be.defined;
+    expect(wrapper.props().style).to.be.defined;
+    expect(wrapper.props().type).to.be.defined;
+    expect(wrapper.props().buttonText).to.be.defined;
+    expect(wrapper.props().buttonIcon).to.be.defined;
+    expect(wrapper.props().onButtonClick).to.be.defined;
+  });
+
+  it ('props contains matching properties', function() {
+    const styleColor = '#ffd400';
+    const wrapper = mount(
+      <Button buttonClass='btn'
+              style={{color: styleColor}}
+              type="button"
+              buttonText="test" />
+    );
+
+    expect(wrapper.props().buttonClass).to.equal('btn');
+    expect(wrapper.props().style.color).to.equal(styleColor);
+    expect(wrapper.props().type).to.equal('button');
+    expect(wrapper.props().buttonText).to.equal('test');
+  });
+
+  it ('has a <button></button> rendered', function() {
+    const wrapper = shallow(<Button />);
+    expect(wrapper.find('button')).to.have.length(1);
+  })
+
+  it ('should simulate click event', function() {
+    const wrapper = mount(<Button onButtonClick={() => {}} />);
+    wrapper.find('button').simulate('click');
   });
 });
-
-/*
-const modCode = 'CS1010S';
-const plannerIDs = getPlannerIDsGivenUserID(userID);
-const semesterIndex = 0;
-
-const module = m_insertOneModuleInSemester.call({
-  semesterIndex: semesterIndex,
-  moduleCode: modCode,
-  plannerID: plannerIDs[0]
-});
-assert.equal(module, modCode);
- */
